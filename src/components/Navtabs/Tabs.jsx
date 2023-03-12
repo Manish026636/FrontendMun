cd0ad54e4fc779433680c90dabae0b6b7ffa5b4f
Manish <my79220@gmail.com>
2023-03-12 16:05:33 +0530

    Done

---
+++ src/components/Navtabs/Tabs.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Tab = ({tabName,handleClick,activeTab}) => {
+  return (
+    
+         <button
+          className={`${
+            activeTab === tabName ? "bg-blue-500 text-white" : "bg-gray-300"
+          } py-2 px-4 rounded-lg`}
+          onClick={() => handleClick(tabName)}
+        >
+         tabName
+        </button>
+    
+  )
+}
+
+export default Tab
